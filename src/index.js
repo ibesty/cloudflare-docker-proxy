f66c1e18573d035283d90fb7ff8b36eb9c9bbfa4
Bestie <11418619+ibesty@users.noreply.github.com>
2024-07-07 18:10:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.tangkunhou.com": dockerHub,
+  "ghcr.tangkunhou.com": "https://ghcr.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.tangkunhou.com": dockerHub,
 };
 
 function routeByHosts(host) {
